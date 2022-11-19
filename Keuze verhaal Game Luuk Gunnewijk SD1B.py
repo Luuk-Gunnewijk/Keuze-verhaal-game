468c49a803d9b8ce43c979a3202171e7250f0c45
Luuk <35917@ma-web.nl>
2022-11-19 19:07:38 +0100

    Keuze verhaal Game Luuk Gunnewijk SD1B.py

---
+++ Keuze verhaal Game Luuk Gunnewijk SD1B.py
@@ -0,0 +1,172 @@
+def S3():
+    print("Stuk 3")
+    print("Je loopt naar de man toe die zijn been mist. Je geeft hem de ticket en zegt zorg dat je thuis komt. Hij bedankt jou met tranen in zijn ogen. Dit voelde als het juiste besluit. Ineens hoor je geschreeuw en besef je dat er een bom knalt. Gelukkig nog ver van jou maar wel dichtbij je hebt haast om naar het vliegtuig te gaan. Gelukkig kom je zo binnen en stijgen jullie op je kijkt nog een keer terug op je land om alleen rook te zien. Je bent blij dat je weg bent. Je beseft je hoe moe je bent na dagen reizen en vluchten voor de terroristen die steeds dichterbij kwamen. Ook al ben je doodsbang dat er nog iets gebeurt,  je valt rustig in slaap.")
+    S5()
+
+def S4():
+    print("Stuk 4")
+    print("je geeft de ticket aan de familie van drie. Met hoop in je gezicht dat je de goede keuze heb gemaakt. Alleen de vader wordt boos, hij wil je hulp niet en vertrouwt niet de ticket die je zojuist aan hem gaf. Hij is boos op je, vloekt je uit, hij vertrouwt niemand en dat is te merken. Voordat je het weet scheurt hij de ticket door midden dit maakt je boos maar voor dat je het weet valt er weer een bom in de buurt. Gelukkig nog ver van jou maar wel dichtbij je hebt haast om naar het vliegtuig te gaan. Gelukkig kom je zo binnen en stijgen jullie op je kijkt nog een keer terug op je land om alleen rook te zien. Je bent blij dat je weg bent. Je beseft je hoe moe je bent na dagen reizen en vluchten voor de terroristen die steeds dichterbij kwamen. Ook al ben je doodsbang dat er nog iets gebeurt, je valt rustig in slaap")
+    S5()
+
+def S5():
+    print("Stuk 5")
+    print("Een paar uur later word je wakker. Of eigenlijk je schrikt wakker van je nachtmerrie. Het was net als of je terug was in Syrië maar dat ben je niet. Je bent gewoon in het vliegtuig. Je kijkt uit het raam het lijkt niet eens op het land waar je vandaan kwam. Het ziet er zo rustig uit. Ineens krijg je dit gevoel als of je bekeken wordt. Je kijkt om je heen achter niks voor je niks misschien maak je je gewoon zorgen. Iets voelt alleen niet goed je kijkt nog een keer naar achteren om zeker te zijn en daar zie je hem je vriend die je heeft verraden. Nu ben je in paniek hoe kan hij op het vliegtuig zijn jij had zijn ticket dit kan niet echt zijn. Alleen dit is echt je loopt naar de wc toe, je kijkt rustig om je heen in de hoop dat je iets van een wapen kan vinden of een uitgang. Je ziet een deur die open kan alleen je bent in een vliegtuig je kan niet zomaar naar buiten springen. Dat is wanneer je het ziet een parachute. Je loopt door naar de wc en denkt na over je keuzes. Je kan er misschien uit springen met de parachute alleen hoe doe je dat en ben je wel hoog genoeg, het is een goot risico. Je kan ook terug gaan naar je plaats je vriend kan toch niks doen er zijn te veel mensen en hij kan niet het vliegtuig overnemen hij kan niet vliegen en er zijn te veel mensen je loopt uit de wc dit is waar je de keuze maakt spring uit het vliegtuig met de parachute of loop je terug naar je plaats. Vraag 3: wat doe je in het vliegtuig.")
+    print("a: spring er uit met de parachute.")
+    print("b: ga terug naar je plek en wacht tot je geland bent.")
+    vraag3 = input(":")
+    if vraag3 == 'a':
+        S19()
+    if vraag3 == 'b':
+        S6()
+
+def S6():
+    print("Stuk 6")
+    print("Je gaat terug naar je plaats je wacht tot je geland bent. Je land in Nederland in de buurt van Amsterdam. Je loopt rustig uit het vliegtuig maar ziet dat je vriend je aan het volgen is. Zodra je op het vliegveld bent probeer je je te verbergen in de groep mensen. Het lijkt er op dat je hem kwijt bent je ziet ook allemaal taxi’s staan, dit zou een manier zijn om hier weg te gaan maar je kan ook blijven wachten. Vraag 4: blijf je wachten tot het nacht is of vlucht je met een taxi?")
+    print("a: vlucht met de taxi")
+    print("b: wacht tot het donker is")
+    vraag4 = input(":")
+    if vraag4 == 'a':
+        S16()
+    if vraag4 == 'b':
+        S7()
+
+def S7():
+    print("Stuk 7")
+    print("Het begint donker te worden en steeds minder mensen zijn er je hebt je vriend ook al heel lang niet meer gezien. Je begint met het lopen naar de uitgang je ziet een taxi maar ook een station. Op de borden van het station staat een route naar Amsterdam. Vraag 4: neem je de taxi of de trein.")
+    print("a: de taxi")
+    print("b: de trein")
+    vraag5 = input(":")
+    if vraag5 == 'a':
+        S8()
+    if vraag5 == 'b':
+        S14()
+
+def S8():
+    print("Stuk 8")
+    print("Je loopt naar de taxi toe en vraagt aan de man of hij je naar een hotel in de buurt kan brengen. De man kijkt je aan of je gek bent. Hij begint zelf te praten en nu kijk jij of hij gek is. Hij praat in een taal die je helemaal niet kent. Je bent nu natuurlijk in Nederland. Je praat geen Nederlands je praat Arabisch en een beetje, heel klein beetje Engels. Voordat je het weet pakt de man een kaart en zet zijn vinger op een de kaart en wijst naar een plaatst die Den haag heet. Misschien dacht hij dat je dat bedoelt je zei iets wat daar op leek. Hij zegt is zoals beautiful city. Als het goed is betekent dat een mooie stad denk je het kan ook beteken dat daar een kasteel is denk je. Je zegt yes. Voor dat je het weet rij je nu met de auto naar deze plaats die heet Den Haag. Benieuwd en zenuwachtig voor waar je nu heen gaat. Het lijkt er op dat je niet wordt gevolgd door je vriend wat het meest belangrijk is. Je rijdt ongeveer 43 minuten voor dat je er bent zodra de auto stopt kijkt de man je aan en steekt hij zijn hand uit naar je toe. Je geeft hem een hand terug je stapt uit en zo stapt ook de man uit boos en vloekt naar je. Dit begrijp je niet hij lijkt steeds bozer te worden.je hoort een van de worden wat hij hier in zegt “politie”. Hier schrik je van. Je denkt nu om weg te rennen of met de man te proberen te praten. Vraag 5: ren je weg of probeer je de man te kalmeren?")
+    print("a: je rent weg")
+    print("b: je probeert de man te kalmeren")
+    vraag6 = input(":")
+    if vraag6 == 'a':
+        S9()
+    if vraag6 == 'b':
+        S10()
+
+def S9():
+    print("Stuk 9")
+    print("Je rent zo snel als je kan door allemaal straten heen tot dat je uit komt bij een plein. Je kijkt rond de man lijkt weg te zijn. Ineens krijg je een schok over je hele lichaam je bent door de politie getaserd. Ze zeggen allemaal dingen tegen je waar je niks van begrijpt voordat je het weet val je bewusteloos.")
+    S11()
+
+def S10():
+    print("Stuk 10")
+    print("Je probeert de man te kalmeren dit leidt nergens heen mensen zien wat er aan de hand is. De man vloekt veel naar je en praat nu heel erg hard voordat je het weet staat er nu ook politie. Zij worden ook snel boos en schieten uiteindelijk met een taser op jou. Hierdoor val je op de grond en word je bewusteloos.")
+    S11()
+
+def S11():
+    print("Stuk 11")
+    print("Je wordt wakker in een politiekamer je hebt handboeien om, twee agent beginnen jou vragen te stellen je kan op niks antwoorden. Je praat hun taal niet. Na een bepaalde tijd loopt een van de agenten weg en komt terug met een derde agent deze man ziet er Arabisch uit maar is wel een Nederlandse agent hij begint met jou te praten en spreekt ook je taal. Hij legt uit dat je de man van de taxi zijn hand uit stak in verwachting dat je hem geld zou geven maar in plaats daarvan gaf je hem een hand terug en liep je weg. Dan man werd natuurlijk boos. In jouw land heb je taxi’s waar voor je niet betaald het idee is dat je wanner je naar de moskee gaat hem bedankt dat je naar je plaats bracht en aan God vraagt of je iets aardigs kan terug geven. Zoals een appel of ander gezond eten. Na dat je uitlegt aan de agent dat je geen geld hebt en dat je de taal niet kent vraagt de agent aan je waarom je bent gevlucht je laat weten dat je met jouw vriend aan het vluchten was voor de oorlog en dat hij jou heeft verraden je legt uit dat je zijn ticket hebt weggegeven maar dat hij nog steeds op het vliegtuig was terecht gekomen zodra je was geland ben je je gaan verstoppen tussen de mensen en heb je gewacht tot het donker was. Zodra het donker was ben je in de taxi gegaan in de hoop dat je ver weg van het vliegveld kon gaan. De agent begrijpt je verhaal hij loopt weg en vertelt aan de man van de taxi het verhaal na een paar minuten komen de agent en de man van de taxi terug. Ze hebben een deal voor je. Het aanbod is dat je de man van de taxi terug betaalt maar niet met geld maar om hem te helpen bij zijn bedrijf deze man heeft namelijk een tweede baan als kapper een kapperszaak die hij zelf heeft opgericht, hij zoekt alleen een schoonmaker. Als je dit accepteert zal hij je ook proberen Nederlands te leren. Dit klinkt als een goed aanbod. Vraag 6: neem je het aanbod aan of weiger je het aanbod?")
+    print("a: je neemt het aanbod aan")
+    print("b: je weigert het aanbod")
+    vraag7 = input(":")
+    if vraag7 == 'a':
+        S12()
+    if vraag7 == 'b':
+        S13()
+
+def S12():
+    print("Stuk 12")
+    print("Je neemt het aanbod aan de man blijkt blij te zijn dat je het aanbod aan neemt. Door de tijd heen begin je Nederlands te spreken je begint ook een goede vriendschap met de man die je dit aanbod heeft gedaan. De Arabische agent komt ook soms langs om te praten. Al in al je leven blijkt eindelijk rustiger te worden. Het beste van dit allemaal je hebt je vriend die je heeft verraden nooit meer gezien.")
+    print("Dit is het einde van jou verhaal")
+
+def S13():
+    print("Stuk 13")
+    print("Je weigert het aanbod. De man van de taxi wordt weer boos. Je kan zien dat de agenten hem proberen te kalmeren. Ondertussen word je zelf in de boeien geslagen. De Arabische agent zegt dat je naar de gevangenis moet voor niet betalen voor de taxi en het niet kunnen betalen van de boete die ze zouden geven als je een baan of geld had. Dit betekent dat je misschien wel bent weg gekomen van je vriend die je heeft verraden maar ook dat je een moeilijk begin hebt in dit nieuwe vreemde land.")
+    print("Dit is het einde van jou verhaal")
+
+def S14():
+    print("Stuk 14")
+    print("Je pakt de trein naar Amsterdam zodra je uitstapt kom je aan op een plein waar het heel druk is. Er is een soort feest wat wordt gegeven. Er zijn honderden mensen dit is goed voor jou als je vriend je aan het volgen was dan zou hij je zo kwijt raken tussen de mensen. Alleen je hebt hem ook al niet meer gezien sinds het vliegtuig. Je kijkt rond bij dit feest en probeert een uitgang te zoeken je wilt graag naar het hotel om te kunnen uitrusten. Je begint alleen helemaal verdwaald te raken.")
+    S15()
+
+def S15():
+    print("Stuk 15")
+    print("Je hoort ineens geschreeuw, mensen zijn aan het rennen. Het is net als of er iets gebeurt dan hoor je een bom afgaan ver van jou en dan een andere achter je maar wel dichterbij en dan zie je hem je vriend hij is hier hij heeft iets in zijn hand het is een bom. Hij ziet jou ook en rent op je af. Je kan niet bewegen je bent zo bang voor dat je het weet springt je vriend naar je toe en laat de bom afgaan. En nu niks.")
+    print("Dit is het einde van jou verhaal")
+
+def S16():
+    print("Stuk 15")
+    print("Je rent naar de taxi toe. Je vertelt tegen de man dat hij zo snel mogelijk moet weg rijden. Hij kijkt je aan of je gek bent hij begrijpt je niet. Je kijkt naar achteren en ziet je vriend op je af rennen hij slaat zo met zijn vuist door het raam dit is wanneer de taxi begint te rijden en snel ook. Je kan zien dat je vriend een auto steelt en achter je aan rijdt. De man van de taxi is in een paniek maar rijdt wel zo snel mogelijk weg. Jij bent ook in paniek maar ziet dat je vriend iets naar je toe gooit een paar seconden later gaat komt er een knal vandaan het was een bom die hij naar jouw gooide. Gelukkig was hij niet raak dit is wel het moment waarop je je beseft dat de man van de taxi niet super snel durft te rijden je zou hem er uit kunnen gooien en zelf door rijden. Vraag 7: gooi je de taxi chauffeur er uit of hoop je dat hij snel genoeg rijdt?")
+    print("a: gooi de taxi chauffeur er uit")
+    print("b: wacht en hoop dat de taxi chauffeur weet wat hij doet")
+    vraag8 = input(":")
+    if vraag8 == 'a':
+        S17()
+    if vraag8 == 'b':
+        S18()
+
+def S17():
+    print("Stuk 17")
+    print("je probeert de taxi chauffeur er uit te gooien, dit lukt nu kun je sneller weg rijden. Het moment dat je de chauffeur er uit gooit rijdt je vriend over hem heen dit leidt je af en rij je direct tegen een muur aan. Hierdoor kan de auto niet meer rijden en gooit je vriend een bom naar de auto toe. Dit is het laatste wat je ziet voor de knal")
+    print("Dit is het einde van jou verhaal")
+
+def S18():
+    print("Stuk 18")
+    print("Je laat de chauffeur zitten en dat was een goed idee ook hij rijdt naar een politiebureau toe. Hij springt uit de auto en roept voor de politie. Hij vergeet alleen het slot er af te halen en jij zit vast in de auto. Je vriend gooit een bom naar de auto toe dit is het laatste wat je ziet voor de knal.")
+    print("Dit is het einde van jou verhaal")
+
+def S19():
+    print("Stuk 19")
+    print("Je pakt de parachute de werknemers worden boos maar voordat zij je kunnen bereiken heb je de deur al open en spring je er uit met je parachute je probeert zo snel mogelijk aan het touw te trekken zodat de parachute open gaat het zit vast dit lukt niet je geeft het nog een kans en bam de parachute is open. Je kijkt naar beneden en je hebt nog de tijd om even te landen. Je landt op een veld van gras je kijkt nog naar boven om het vliegtuig te zien weg vliegen je bent hem ontsnapt denk je in de verte zie je een huis je loopt daar naar toe en klopt aan bij het huis een man die er uit ziet als een dikke cowboy doet open. Je legt uit dat je zojuist uit het vliegtuig bent gesprongen om te vluchten van je vriend. De man kijkt je aan of je gek bent het is natuurlijk ook een gek verhaal. Zodra hij begint te praten kijk jij of je gek bent hij praat niet jouw taal er komt ook en jongere man naar de deur toe je probeert het aan hem uit te leggen. Hij lijkt je te begrijpen hij heeft zelf een paar lessen gehad in het Arabisch hij vertelt zijn vader dat je bent gevlucht en of je hier een paar dagen kan blijven. De vader geeft antwoord in het Nederlands aan de zoon. De zoon vertelt dat je hier een paar dagen kan blijven of dat zijn vader hem naar Amsterdam zou kunnen brengen om daar een leven te beginnen. Vraag 8: blijf je een paar dagen met de mensen hier of ga je met de man mee die je afzet bij Amsterdam?")
+    print("a: je blijft een paar dagen ")
+    print("b: je laat je afzetten bij Amsterdam")
+    vraag9 = input(":")
+    if vraag9 == 'a':
+        S20()
+    if vraag9 == 'b':
+        S23()
+
+def S20():
+    print("Stuk 20")
+    print("Je blijft een paar dagen en begint te wennen aan het leven op deze boerderij. De zoon probeert je ook Nederlands te leren wat wel aardig gaat, je helpt met de boerderij en krijgt zelfs geld van de vader dit voelt als een goed begin van een nieuw leven na twee maanden vraagt de vader en de zoon of je hier zou willen wonen en wilt blijven helpen op de boerderij je bent namelijk goede vrienden geworden met de mensen hier. Ze zeggen ook dat ze je kunnen helpen met een huis vinden. Vraag 9: ga je een huis zoeken of blijf je op de boerderij werken?")
+    print("a: je blijft op de boerderij werken")
+    print("b: je laat ze helpen met het zoeken naar een nieuw huis")
+    vraag10 = input(":")
+    if vraag10 == 'a':
+        S21()
+    if vraag10 == 'b':
+        S22()
+
+def S21():
+    print("Stuk 21")
+    print("Je blijft werken op de boerderij en helpt met werk op de boerderij je moet soms wel terug denken aan je vriend die je heeft verraden maar je bent blij dat je hem nooit meer gaat zien.")
+    print("Dit is het einde van jou verhaal")
+
+def S22():
+    print("Stuk 22")
+    print("Je laat ze helpen bij het vinden van een nieuw huis, je gaat wonen in Den Haag. Het is zeker wel wennen maar het leven is beter je hebt ook een baan bij een kapperszaak in de buurt als schoonmaker. Het leven kan soms lastig zijn en soms moet je terug denken over je vriend die je heeft verraden maar hem ga je nooit meer zien.")
+    print("Dit is het einde van jou verhaal")
+
+def S23():
+    print("Stuk 23")
+    print("De vader neemt je mee naar Amsterdam en zet je af bij dit feest met honderden mensen. Je loopt door alle mensen heen en raakt al snel verdwaald je zoekt eigenlijk een hotel. Voor dat je het weet, weet je niet meer waar je bent. ")
+    S15()
+
+print("Stuk 1")
+print("Je woont in Syrië")
+naam = input("Wat is je naam:")
+print("Je wordt wakker van een andere knal. Dit is niet de eerste keer dat je wakker wordt van een bom. Alleen deze is wel dichtbij,  je maakt dus je vriend wakker en samen vluchten jullie naar het vliegveld. Nog 12 kilometer lopen. Tijdens het lopen blijft je vriend dingen zeggen om je misschien over te halen om te blijven. Dit deed hij gisteren ook al en de dag daarvoor ook al. Elke dag wordt het erger alsof hij niet weg wil. Je bent er eigenlijk klaar mee. Vraag 1: word je boos op je vriend. Of probeer je het nog te negeren")
+print("a: Boos worden")
+print("b: Negeren")
+vraag1 = input(":")
+
+print("stuk 2")
+print("Het maakt niet uit. Je vriend blijft er over door gaan. Jullie zijn nog iets van 300 meter van het vliegveld af. Op dit moment zegt je vriend iets waarvan je schrikt. Hij gelooft in de terroristen en hun reden voor deze oorlog. Dit is waarom hij de hele tijd wou dat jullie bleven. Maar het wordt erger, hij wordt agressief. Hij dreigt je dood te maken. Dit moment ren je het laatste stuk naar het vliegveld. Je rent voor je leven maar je kan het idee niet los laten dat je zojuist bent verraden door iemand die je vertrouwde. Hoe dan ook je bent bij het vliegveld het is erg druk veel mensen willen vluchten. Gelukkig heb je twee vlieg tickets en kom je er zo door heen. Je kijkt nog rond of je je vriend kan vinden door de mensen. Maar wat je ziet is een man die zijn been is kwijt geraakt het verband ziet er nog nieuw uit als of het net is gebeurd. Naast hem staat een bord met daarop “al het geld wat je mij kan geven zodat ik een ticket kan betalen voor een vliegtuig wordt gewaardeerd . Ik moet terug naar mijn familie.” Je loopt naar hem toe om de ticket te geven die je nog over hebt van je vriend. Voor dat je bij hem in de buurt bent zie je een familie van drie een vader, moeder en een kind van vijf. Ze zijn ruzie aan het maken met de medewerker ze zijn een ticket kwijt geraakt. Dit betekent dat ze niet met zijn drieën op het vliegtuig kunnen. Je hebt twee tickets eentje voor jezelf maar aan wie geef je de andere tickets. Vraag 2: aan wie geef je de laatste ticket?")
+print("a: de man die een been mist")
+print("b:de familie van drie")
+vraag2 = input(":")
+
+if vraag2 == 'a':
+    S3()
+
+if vraag2 == 'b':
+    S4()
